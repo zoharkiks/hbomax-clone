dfdee5c79efb5c3f6d33f9d03c2f027ed3a376bc
zoharkiks <zoharkiks@gmail.com>
2022-08-01 21:19:51 +0530

    Stop building hero section

--- containers/Hero.tsx
+++ containers/Hero.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
+import { Icon } from "@iconify/react";
 
 const Hero = () => {
   return (
-    <div>I'am a Hero Section</div>
-  )
-}
+    <div className="mt-14 flex flex-col items-start justify-center font-gilmed text-white">
+      <h1 className="font-gilbold text-2xl">DRACULA</h1>
 
-export default Hero
+      <div className="mt-2 flex items-center justify-center space-x-2 text-sm">
+        <Icon className="h-9 w-9" icon="la:imdb" color="#f6c700" />
+        <span>8.5</span>
+      </div>
+      <span>Action | Adventure | Fantasy</span>
+    </div>
+  );
+};
+
+export default Hero;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,26 +1,25 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
+import type { NextPage } from "next";
+import Head from "next/head";
 
 // import containers
-import {Footer,Hero}  from '../containers'
-// import components 
-import {Navbar}  from '../components'
+import { Footer, Hero } from "../containers";
+// import components
+import { Navbar } from "../components";
 
 const Home: NextPage = () => {
   return (
-    <div className="p-3 md:px-8 lg:px-12 h-screen bg-black bg-no-repeat bg-contain md:bg-cover bg-[url('https://www.teahub.io/photos/full/67-670663_hollywood-movie-poster-hd.jpg')]" >
+    <div className="h-screen  bg-black bg-[url('https://www.teahub.io/photos/full/67-670663_hollywood-movie-poster-hd.jpg')] bg-contain bg-no-repeat p-3 md:bg-cover md:px-8 lg:px-12">
       <Head>
         <title>HBO Max Clone</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-{/* Website Sections */}
-<Navbar/>
-<Hero/>
-<Footer/>
-
+      {/* Website Sections */}
+      <Navbar />
+      <Hero />
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
