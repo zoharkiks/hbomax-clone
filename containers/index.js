043e7cb2283ba91c24db7695d83e9fa645fa536e
zoharkiks <zoharkiks@gmail.com>
2022-08-01 18:30:49 +0530

    Move navbar to component folder

--- containers/index.js
+++ containers/index.js
@@ -1,7 +1,6 @@
 // Import all containers here for easy exports
 
-import  Navbar  from "./Navbar";
 import  Footer  from "./Footer";
 import  Hero  from "./Hero";
 
-export {Navbar, Footer, Hero}
+export {Footer, Hero}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,10 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 
-// import components
-import {Navbar, Footer,Hero}  from '../containers'
+// import containers
+import {Footer,Hero}  from '../containers'
+// import components 
+import {Navbar}  from '../components'
 
 const Home: NextPage = () => {
   return (
