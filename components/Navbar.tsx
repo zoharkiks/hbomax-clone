706809a33560f0bc8e275bdaf225a1800dcf055b
zoharkiks <zoharkiks@gmail.com>
2022-08-01 21:01:59 +0530

    Add navbar component and navbar breakpoints

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,97 @@
-import React from 'react'
+import React, { useState } from "react";
+import images from "../constants/images";
+import { Icon } from "@iconify/react";
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+  const [opened, setOpened] = useState(false);
 
-export default Navbar
+  const toggleMenu = () => {
+    setOpened(!opened);
+  };
+
+  return (
+    <div className="flex items-center justify-between rounded-[20px] bg-black bg-opacity-40  p-6 font-gilmed backdrop-blur-[10px] md:px-12   ">
+      <img className="" src={images.logo.src} alt="logo" />
+      <Icon
+        onClick={toggleMenu}
+        className="h-5 w-5 cursor-pointer text-white md:hidden"
+        icon="typcn:th-menu"
+      />
+
+      <ul className="hidden md:flex space-x-6 text-white lg:space-x-12">
+        <li>
+          <a href="">Movies</a>
+        </li>
+        <li>
+          <a href="">TV Shows</a>
+        </li>
+        <li>
+          <a href="">Animations</a>
+        </li>
+      </ul>
+
+      <img
+        className="h-12 w-12 rounded-full object-cover hidden md:block cursor-pointer "
+        src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZmlsZXxlbnwwfHwwfHw%3D&w=1000&q=80"
+        alt=""
+      />
+
+      {opened ? (
+        <div className="absolute right-0 top-0 w-max flex-col rounded-[20px] bg-black bg-opacity-40 py-6 px-4 text-white backdrop-blur-[200px]  ">
+          <div className="mb-6 flex items-center justify-between ">
+            <img className="" src={images.logo.src} alt="logo" />
+            <Icon
+              className="h-5 w-5 text-white"
+              icon="ant-design:close-circle-filled"
+              onClick={toggleMenu}
+            />
+          </div>
+
+          <form className="mb-12 flex items-center justify-between  rounded-[10px] bg-white bg-opacity-60 p-3 ">
+            <Icon
+              className="mr-5 h-6  w-6 text-white"
+              icon="ant-design:search-outlined"
+            />
+            <input
+              placeholder="Search"
+              className="bg-transparent text-black  outline-none placeholder:text-black"
+              type="text"
+            />
+          </form>
+
+          <ul className=" space-y-5">
+            <li>
+              <a href="">Movies</a>
+            </li>
+            <li>
+              <a href="">TV Shows</a>
+            </li>
+            <li>
+              <a href="">Animations</a>
+            </li>
+          </ul>
+
+          <div className="mt-24 flex justify-between">
+            <div className="flex   ">
+              <img
+                className="h-12 w-12 rounded-full object-cover "
+                src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZmlsZXxlbnwwfHwwfHw%3D&w=1000&q=80"
+                alt=""
+              />
+            </div>
+            <div className="flex flex-col">
+              <span>Zohar Williams</span>
+              <span>zohar@gmail.com</span>
+            </div>
+            <Icon
+              className="h-6 w-6 cursor-pointer text-white"
+              icon="humbleicons:logout"
+            />
+          </div>
+        </div>
+      ) : null}
+    </div>
+  );
+};
+
+export default Navbar;
--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,7 @@ import {Navbar}  from '../components'
 
 const Home: NextPage = () => {
   return (
-    <div >
+    <div className="p-3 md:px-8 lg:px-12 h-screen bg-black bg-no-repeat bg-contain md:bg-cover bg-[url('https://www.teahub.io/photos/full/67-670663_hollywood-movie-poster-hd.jpg')]" >
       <Head>
         <title>HBO Max Clone</title>
         <link rel="icon" href="/favicon.ico" />
