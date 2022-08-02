c08d87023a2f93f89c7f4361434efb5e53a48f1d
zoharkiks <zoharkiks@gmail.com>
2022-08-02 11:46:01 +0530

    Add hero section and breakpoints

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,12 +1,12 @@
 import { Icon } from '@iconify/react'
 import React from 'react'
 
-const Button = ({icon,title}:{icon:string, title:string}) => {
+const Button = ({icon,title,padding}:{icon:string, title:string,padding:string}) => {
   return (
     <>
-    <button className='border rounded-[10px] p-2.5 flex space-x-2.5 justify-center items-center font-gilmed '>
+    <button className={`border rounded-[10px] ${padding} flex space-x-2.5 justify-center items-center font-gilmed` }>
     <Icon className='h-5 w-5' icon={`${icon}`} />
-<span className='text-sm '>{title}</span>
+<span className='text-sm md:text-lg lg:text-xl '>{title}</span>
 
     </button>
     </>
--- containers/Hero.tsx
+++ containers/Hero.tsx
@@ -4,18 +4,19 @@ import { Button } from "../components";
 
 const Hero = () => {
   return (
-    <div className="mt-14 flex flex-col items-start justify-center font-gilmed text-white">
-      <h1 className="font-gilbold text-2xl">DRACULA</h1>
+    <div className="mt-10 md:mt-14 flex flex-col items-start justify-center font-gilmed text-white">
+      <h1 className="font-gilbold text-2xl md:text-[2.5rem] lg:text-[5.5rem] w-1/2 leading-[3rem] lg:leading-[6rem] uppercase">Spider-Man: 
+No Way Home</h1>
 
-      <div className="mt-2 flex items-center justify-center space-x-2 text-sm">
-        <Icon className="h-9 w-9" icon="la:imdb" color="#f6c700" />
+      <div className="mt-2 flex items-center justify-center space-x-2 text-sm md:text-lg lg:order-">
+        <Icon className="h-9 w-9 md:h-12 md:w-12 " icon="la:imdb" color="#f6c700" />
         <span>8.5</span>
       </div>
-      <span className="mt-3">Action | Adventure | Fantasy</span>
-      <span className="mt-2">2021 | PG-13 | 2h 28m</span>
+      <span className="mt-3 md:text-xl lg:text-3xl">Action | Adventure | Fantasy</span>
+      <span className="mt-2 md:order-first md:mt-0 md:mb-4 lg:text-xl">2021 | PG-13 | 2h 28m</span>
 
       <div className="mt-7">
-        <Button title="Watch Now" icon="ci:play-circle-outline" />
+        <Button title="Watch Now" icon="ci:play-circle-outline" padding='p-2.5 md:p-4 lg:p-6' />
       </div>
     </div>
   );
