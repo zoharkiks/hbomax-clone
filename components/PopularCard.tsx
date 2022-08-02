edc6eb4dbdf3f3a6622a2340525ba3cdede4e273
zoharkiks <zoharkiks@gmail.com>
2022-08-02 12:03:30 +0530

    Add Popular container and Popular Card component with breakpoints

--- components/PopularCard.tsx
+++ components/PopularCard.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const PopuplarCard = () => {
   return (
-    <div>PopuplarCard</div>
+    <>
+    <img className='w-[108px] h-[139px] md:w-[148px] md:h-[179px] lg:w-[219px] lg:h-[280px] object-cover rounded-xl' src="https://m.media-amazon.com/images/M/MV5BODk2NjAyOWMtM2FjZC00MjZhLTkxMjQtZTM3NjJlYTE5MDdlXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_FMjpg_UX1000_.jpg" alt="" />
+    </>
   )
 }
 
--- containers/Popular.tsx
+++ containers/Popular.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import { PopularCard } from '../components'
 
 const Popular = () => {
   return (
     <div className='text-white mt-20 flex flex-col font-gilbold text-lg'>
-<h1>Popular Collections</h1>
+<h1 className='md:text-[2.5rem] md:text-xl lg:text-3xl'>Popular Collections</h1>
+<div className="flex mt-4 space-x-5">
+<PopularCard/>
+<PopularCard/>
+
+</div>
     </div>
   )
 }
