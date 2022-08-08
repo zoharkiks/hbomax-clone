04245ee3c3b3e338bf79d92db09e663ffbcade74
zoharkiks <zoharkiks@gmail.com>
2022-08-08 13:33:28 +0530

    Test 7

--- containers/NewReleases.tsx
+++ containers/NewReleases.tsx
@@ -29,7 +29,6 @@ const NewReleases = () => {
     <div className="mt-8 flex flex-col font-gilbold text-lg text-white">
       <h1 className="md:text-xl lg:text-3xl">Upcoming Movies</h1>
       <div className="mt-5 flex">
-        <Flicking moveType="freeScroll" align="prev" circular={true}>
           {upcomingMovies?.slice(0, 16).map((movie: any) => (
             <div key={movie?.id} className="mr-4 w-[40%] md:w-[37%] lg:w-[25F%]  ">
               <NewReleasesCard
@@ -40,7 +39,7 @@ const NewReleases = () => {
               />
             </div>
           ))}
-        </Flicking>
+        
       </div>
     </div>
   );
