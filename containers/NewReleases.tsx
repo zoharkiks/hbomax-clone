4f8b5d4baa6ca3535ac56d5bf1a00a0bcd098157
zoharkiks <zoharkiks@gmail.com>
2022-08-08 18:48:01 +0530

    test

--- containers/NewReleases.tsx
+++ containers/NewReleases.tsx
@@ -32,16 +32,16 @@ const NewReleases = () => {
       <h1 className="md:text-xl lg:text-3xl">Upcoming Movies</h1>
       <div className="mt-5 flex">
         <Flicking moveType="freeScroll" align="prev" circular={true}>
-          <div className="mr-4 flex  ">
           {upcomingMovies?.slice(0, 3).map((movie: any) => (
+            <div key={movie?.id} className="mr-4 flex  ">
               <NewReleasesCard
                 key={movie?.id}
                 title={movie.title}
                 image={`https://image.tmdb.org/t/p/w500${movie?.backdrop_path}`}
                 clickFunction={() => fetchDetails(movie.id, movie.title)}
               />
-              ))}
-              </div>
+            </div>
+          ))}
         </Flicking>
       </div>
     </div>
