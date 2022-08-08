f74b914c52952da2bf3b2cdd71a6f725f64c4b48
zoharkiks <zoharkiks@gmail.com>
2022-08-08 13:39:25 +0530

    Test 9

--- containers/NewReleases.tsx
+++ containers/NewReleases.tsx
@@ -9,6 +9,7 @@ import "@egjs/react-flicking/dist/flicking.css";
 import { useSelector, useDispatch } from "react-redux";
 // Reducers
 import { getDetails, getUpcomingMovies } from "../features/movies/moviesSlice";
+import PopuplarCard from "../components/PopularCard";
 
 const NewReleases = () => {
   const router = useRouter();
@@ -29,17 +30,23 @@ const NewReleases = () => {
     <div className="mt-8 flex flex-col font-gilbold text-lg text-white">
       <h1 className="md:text-xl lg:text-3xl">Upcoming Movies</h1>
       <div className="mt-5 flex">
+        <Flicking moveType="freeScroll" align="prev" circular={true}>
           {upcomingMovies?.slice(0, 16).map((movie: any) => (
-            <div key={movie?.id} className="mr-4 w-[40%] md:w-[37%] lg:w-[25F%]  ">
-              <NewReleasesCard
+            <div key={movie?.id} className="mr-4  ">
+              {/* <NewReleasesCard
                 key={movie?.id}
                 title={movie.title}
                 image={`https://image.tmdb.org/t/p/w500${movie?.backdrop_path}`}
                 clickFunction={() => fetchDetails(movie.id, movie.title)}
+              /> */}
+              <PopuplarCard
+                key={movie?.id}
+                image={`https://image.tmdb.org/t/p/w300/${movie.poster_path}`}
+                clickFunction={() => fetchDetails(movie.id, movie.title)}
               />
             </div>
           ))}
-        
+        </Flicking>
       </div>
     </div>
   );
